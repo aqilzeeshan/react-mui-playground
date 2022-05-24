d211132968fd2ac2ec1d9aa56cf2515e8b8a7560
Aqil Zeeshan <azeeshan@freewheel.com>
2022-05-24 15:55:18 +0100

    Using AppBar

---
+++ src/components/ui/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+
+function ElevationScroll(props) {
+    const { children} = props;
+
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0,
+    });
+  
+    return React.cloneElement(children, {
+      elevation: trigger ? 14 : 0,
+    });
+  }
+
+export const Header = (props) => {
+  return (
+    <ElevationScroll>
+        <AppBar position='fixed'>
+            <Toolbar>
+                BoardGame Swap
+            </Toolbar>
+        </AppBar>
+    </ElevationScroll>
+  )
+}
