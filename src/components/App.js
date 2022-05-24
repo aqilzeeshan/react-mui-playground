3e54608c02a5f2468f3ed8b4bcded70883fd8d44
Aqil Zeeshan <azeeshan@freewheel.com>
2022-05-24 22:58:32 +0100

    Text + image in app bar

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,16 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Header/>
-
+        <Box sx={{ my: 10 }}>
+          {[...new Array(12)]
+            .map(
+              () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+            )
+            .join('\n')}
+        </Box>
     </ThemeProvider>
   );
 }
